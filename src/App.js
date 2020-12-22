eb51e2cbf47a4301dd52fed120dcc9359e8bdee2
raghav <rgv3300@gmail.com>
2020-12-21 21:53:30 -0500

    About us Section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+
+//import pages
+import AboutUs from "./pages/AboutUs";
+import AboutSection from "./components/AboutSection";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AboutUs />
+    </div>
+  );
 }
 
 export default App;
